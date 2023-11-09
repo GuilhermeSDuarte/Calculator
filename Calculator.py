f777bef2af2f8170385ffb032720e9362c620284
Guilherme Duarte <57077716+GuilhermeSDuarte@users.noreply.github.com>
2023-11-08 22:40:12 -0300

    Inicio

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+
+def main():
+    escolha = int
+
+
+def operacoes(escolha, valor1, valor2):
+    if escolha == "+":
+        resultado = valor1 + valor2
+    elif escolha == "-":
+        resultado = valor1 - valor2
+    elif escolha == "*":
+        resultado = valor1 * valor2
+    elif escolha == "/":
+        resultado = valor1 / valor2
+    elif escolha == "%":
+        resultado = valor1 / 100
