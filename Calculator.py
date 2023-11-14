661d52c337ec23d9daac612551f55ec33b0dc719
Guilherme Duarte <57077716+GuilhermeSDuarte@users.noreply.github.com>
2023-11-13 22:24:17 -0300

    Tentativa de implementação aos calculos

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,25 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.bgcolor = "#363636"
     page.window_resizable = False
     page.window_width = 300
-    page.window_height = 400
+    page.window_height = 350
     page.title = "Calculadora"
-    page.vertical_alignment.SPACE_EVENLY
     page.window_always_on_top = True
+    result = ft.Text(value="", color="#FFFFFF", size=30)
 
-    result = ft.Text(value="0", color="#FFFFFF", size=20)
+
+    def calculate():
+        pass
+
+
+    def keyboard(e):
+        pass
+    
 
     page.add(
-
         ft.Row(
             width=300,
             alignment="end",
@@ -23,10 +30,10 @@ def main(page: ft.Page):
             expand=1,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.ElevatedButton(text="%", bgcolor="#000000"),
-                ft.ElevatedButton(text="AC", bgcolor="#000000"),
-                ft.ElevatedButton(text="C", bgcolor="#000000"),
-                ft.ElevatedButton(text="/", bgcolor="#000000")]
+                ft.ElevatedButton(text="C", bgcolor="#000000", on_click=keyboard),
+                ft.ElevatedButton(text="<", bgcolor="#000000", data="<", on_click=keyboard),
+                ft.ElevatedButton(text="+/-", bgcolor="#000000", data="+/-", on_click=keyboard),
+                ft.ElevatedButton(text="/", bgcolor="#000000", data="/", on_click=keyboard)]
         ),
         ft.Row(
             width=300,
@@ -34,44 +41,43 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
 
             controls=[
-                ft.ElevatedButton(text="7", bgcolor="#1C1C1C",style=ft.ButtonStyle(
-                shape=ft.CircleBorder(3)
-            )),
-                ft.ElevatedButton(text="8", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="9", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="x", bgcolor="#000000")]
+                ft.ElevatedButton(text="7", bgcolor="#1C1C1C", data="7", on_click=keyboard),
+                ft.ElevatedButton(text="8", bgcolor="#1C1C1C", data="8", on_click=keyboard),
+                ft.ElevatedButton(text="9", bgcolor="#1C1C1C", data="9", on_click=keyboard),
+                ft.ElevatedButton(text="x", bgcolor="#000000", data="*", on_click=keyboard)]
         ),
         ft.Row(
             width=300,
             expand=1,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.ElevatedButton(text="4", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="5", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="6", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="-", bgcolor="#000000")]
+                ft.ElevatedButton(text="4", bgcolor="#1C1C1C", data="4", on_click=keyboard),
+                ft.ElevatedButton(text="5", bgcolor="#1C1C1C", data="5", on_click=keyboard),
+                ft.ElevatedButton(text="6", bgcolor="#1C1C1C", data="6", on_click=keyboard),
+                ft.ElevatedButton(text="-", bgcolor="#000000", data="-", on_click=keyboard)]
         ),
         ft.Row(
             width=300,
             expand=1,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.ElevatedButton(text="1", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="2", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="3", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text="+", bgcolor="#000000")]
+                ft.ElevatedButton(text="1", bgcolor="#1C1C1C", data="1", on_click=keyboard),
+                ft.ElevatedButton(text="2", bgcolor="#1C1C1C", data="2", on_click=keyboard),
+                ft.ElevatedButton(text="3", bgcolor="#1C1C1C", data="3", on_click=keyboard),
+                ft.ElevatedButton(text="+", bgcolor="#000000", data="+", on_click=keyboard)]
         ),
         ft.Row(
             width=300,
             expand=1,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.ElevatedButton(text="+/-", bgcolor="#000000"),
-                ft.ElevatedButton(text="0", bgcolor="#1C1C1C"),
-                ft.ElevatedButton(text=",", bgcolor="#000000"),
-                ft.ElevatedButton(text="=", bgcolor="#000000")]
+                ft.ElevatedButton(text="%", bgcolor="#000000", data="/", on_click=keyboard),
+                ft.ElevatedButton(text="0", bgcolor="#1C1C1C", data="0", on_click=keyboard),
+                ft.ElevatedButton(text=",", bgcolor="#000000", data=".", on_click=keyboard),
+                ft.ElevatedButton(text="=", bgcolor="#000000", data="=", on_click=keyboard)]
         )
 
     )
+
 
 ft.app(target=main)
